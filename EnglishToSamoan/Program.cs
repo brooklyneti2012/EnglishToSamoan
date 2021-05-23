ef0a4e45c385fc815432f18ae50305dd28a0fbeb
Brooklyn Eti <ac106494@avcol.school.nz>
2021-05-24 09:31:09 +1200

    Add project files.

---
+++ EnglishToSamoan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnglishToSamoan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the English to Samoan translator");
+
+            Console.WriteLine("Pick a number between 1 and 5 depending on your skill with the Samoan language");
+
+            string answer = Console.ReadLine();
+
+            if (answer = "1")
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}

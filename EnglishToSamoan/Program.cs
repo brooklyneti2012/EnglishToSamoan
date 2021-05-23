59ae6c32f6597c0d6bbc9e3e9748c166b56f5917
Brooklyn Eti <ac106494@avcol.school.nz>
2021-05-24 09:54:09 +1200

    Static voids

--- EnglishToSamoan/Program.cs
+++ EnglishToSamoan/Program.cs
@@ -12,12 +12,55 @@ namespace EnglishToSamoan
 
             Console.WriteLine("Pick a number between 1 and 5 depending on your skill with the Samoan language");
 
-            string answer = Console.ReadLine();
+            int answer;
+            int.TryParse(Console.ReadLine(), out answer);
 
-            if (answer = "1")
+            switch (answer)
             {
-                Console.WriteLine("");
+                case 5:
+                    FVE();
+                    break;
+                case 4:
+                    FOUR();
+                    break;
+                case 3:
+                    THREE();
+                    break;
+                case 2:
+                    TWO();
+                    break;
+                case 1:
+                    ONE();
+                    break;
+                    
+
             }
+                
+            static void FVE()
+            {
+
+            }
+
+            static void FOUR()
+            {
+
+            }
+
+            static void THREE()
+            {
+
+            }
+
+            static void TWO()
+            {
+
+            }
+
+            static void ONE()
+            {
+                Console.WriteLine("Welcome to level 1/5 of the Samoan tutorial");
+            }
+          
         }
     }
 }

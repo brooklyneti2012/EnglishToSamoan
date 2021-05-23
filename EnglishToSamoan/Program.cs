1f92a177e36799267b75a5f4a2d34ad722f00891
Brooklyn Eti <ac106494@avcol.school.nz>
2021-05-24 10:20:00 +1200

    LEVEL 1

--- EnglishToSamoan/Program.cs
+++ EnglishToSamoan/Program.cs
@@ -8,7 +8,7 @@ namespace EnglishToSamoan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the English to Samoan translator");
+            Console.WriteLine("Welcome to the Samoan tutorial");
 
             Console.WriteLine("Pick a number between 1 and 5 depending on your skill with the Samoan language");
 
@@ -53,14 +53,31 @@ namespace EnglishToSamoan
 
             static void TWO()
             {
-
+                
             }
 
             static void ONE()
             {
                 Console.WriteLine("Welcome to level 1/5 of the Samoan tutorial");
+                Console.WriteLine("In this tutorial you will be learning a simple conversation starter");
+                Console.WriteLine("Press ENTER to continue");
+                Console.ReadLine();
+                Console.WriteLine("The greeting used most of is 'Talofa'(tah - loh - fah)");
+                Console.WriteLine("An simple example of this greeting used in a sentence is...");
+                Console.WriteLine("");
+                Console.WriteLine("Talofa, my name is John.");
+                Console.WriteLine("___________________________________________________________");
+                Console.WriteLine("Press ENTER to continue to excercise");
+                Console.ReadLine();
+                Console.WriteLine("This time write the example sentence but with your name");
+                Console.WriteLine("Talofa, my name is"); 
+                string eg = Console.ReadLine();
+
+
+                Console.ReadKey();
+                
             }
-          
+            
         }
     }
 }

16c0ce51162ea7c12347d2ed3fa5e8a3bb336a1d
Brooklyn Eti <ac106494@avcol.school.nz>
2021-06-18 10:27:45 +1200

    Finished quiz for level two

--- EnglishToSamoan/TWOQUIZ.cs
+++ EnglishToSamoan/TWOQUIZ.cs
@@ -31,7 +31,7 @@ namespace EnglishToSamoan
                 string please1 = Console.ReadLine();
                 please1 = please1.ToLower();
 
-                while (please1 == "c")
+                while (please1 == "c" )
                 {
                     Console.Clear();
                     Console.WriteLine("That is correct press enter to move onto the next question.");
@@ -67,7 +67,7 @@ namespace EnglishToSamoan
                 string lelei1 = Console.ReadLine();
                 lelei1 = lelei1.ToLower();
 
-                if (lelei1 == "lelei")
+                if (lelei1 == "lelei" && lelei1 == "manaia")
                 {
                     Console.WriteLine("That is correct, press enter to continue.");
                     Console.ReadLine();
@@ -109,6 +109,7 @@ namespace EnglishToSamoan
             }
             static void Q3()
             {
+                Console.Clear();
                 Console.WriteLine("Q3 . Multichoice");
                 Console.WriteLine("Which of the following is the definition of 'Leaga' in English?");
                 Console.WriteLine("---------------------------------------------------------------");
@@ -149,7 +150,66 @@ namespace EnglishToSamoan
                 }
                 static void Q4()
                 {
-                    Console.WriteLine("Q4. ");
+                    Console.Clear();
+                    Console.WriteLine("Q4. Written Answer");
+                    Console.WriteLine("How do you say 'Sorry' in Samoan?");
+                    Console.WriteLine("Type your answer, and press enter to continue.");
+                    string malie = Console.ReadLine();
+                    malie = malie.ToLower();
+
+                    if (malie == "malie")
+                    {
+                        Console.WriteLine("That is correct, press enter to finish quiz");
+                        Console.ReadLine();
+                        learnt();
+                    }
+                    else 
+                    {
+                        Console.WriteLine("That answer is incorrect");
+                        Console.WriteLine("Do you want a hint?");
+                        Console.WriteLine("yes or no");
+                        Console.WriteLine("Press enter after making your answer.");
+                        string hint = Console.ReadLine();
+                        hint = hint.ToLower();
+
+                        if (hint == "yes")
+                        {
+                            hint2();
+                        }
+                        else if(hint == "no")
+                        {
+                            Q4();
+                        }
+                    }
+                    static void hint2()
+                    {
+                        Console.Clear();
+                        Console.WriteLine("These are your options.");
+                        string[] hint2;
+                        hint2 = new string[] { "Lelei", "Malie", "Fa'amolemole", "Leaga" };
+                        Console.WriteLine("option 1 = " + hint2[0]);
+                        Console.WriteLine("option 2 = " + hint2[1]);
+                        Console.WriteLine("option 3 = " + hint2[2]);
+                        Console.WriteLine("option 4 = " + hint2[3]);
+                        Console.WriteLine("Press enter to try the question again.");
+                        Console.ReadLine();
+                        Q2();
+                    }
+                }
+                static void learnt()
+                {
+                    Console.Clear();
+                    Console.WriteLine("Congratulations you have officially finished the second tutorial of the Samoan Tutorials Point.");
+                    Console.WriteLine("This is what youve learnt this lesson");
+                    string[] learnt;
+                    learnt = new string[] { "Lelei", "Malie", "Fa'amolemole", "Leaga" };
+                    Console.WriteLine("Good = " + learnt[0] + ", Manaia");
+                    Console.WriteLine("Sorry = " + learnt[1]);
+                    Console.WriteLine("Please = " + learnt[2]);
+                    Console.WriteLine("Bad = " + learnt[3]);
+                    Console.WriteLine("Press enter to move onto the next tutorial.");
+                    Console.ReadLine();
+                    LVLTHREE L3 = new LVLTHREE();
                 }
 
 

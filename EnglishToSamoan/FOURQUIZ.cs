e0346f33ae447c3efe06b884c5fe127637a984de
Brooklyn Eti <ac106494@avcol.school.nz>
2021-07-09 08:27:03 +1200

    started four quiz

--- EnglishToSamoan/FOURQUIZ.cs
+++ EnglishToSamoan/FOURQUIZ.cs
@@ -14,6 +14,16 @@ namespace EnglishToSamoan
             {
                 Console.Clear();
                 Console.WriteLine("Welcome to the End Of Tutorial Quiz for tutorial 4");
+                Console.WriteLine("In this Quiz you will be answer 10 questions");
+                Console.WriteLine("and when you get all the questions right you will move onto the final Tutorial ");
+                Console.WriteLine("Press enter to start the quiz.");
+                Console.ReadLine();
+                que1();
+
+                static void que1()
+                {
+                    Console.WriteLine("");
+                }
 
             }
         }

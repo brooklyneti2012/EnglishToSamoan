50a2ac3b628612d19819f3cfdc66b9763ef61c9b
Brooklyn Eti <ac106494@avcol.school.nz>
2021-07-18 14:25:00 +1200

    working on level 5

--- EnglishToSamoan/LVLFIVE.cs
+++ EnglishToSamoan/LVLFIVE.cs
@@ -8,7 +8,53 @@ namespace EnglishToSamoan
     {
         public LVLFIVE()
         {
+            Console.Clear();
+            Console.WriteLine("Welcome to the FINAL tutorial for the Samoan Tutorials Point.");
+            Console.WriteLine("In this tutorial you will learn how to say the words...");
+            Console.WriteLine("Big");
+            Console.WriteLine("Small");
+            Console.WriteLine("Wide");
+            Console.WriteLine("Skinny");
+            Console.WriteLine("Tall");
+            Console.WriteLine("Short.");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            BIG();
 
+            static void BIG()
+            {
+                Console.Clear();
+                Console.WriteLine("In Samoan, to say the word 'Big' we say. \n 'Lapo'a'");
+                Console.WriteLine("Pronounciation: Lah - poh - ah");
+                Console.WriteLine("Try typing 'Lapo'a' yourself.");
+                string big = Console.ReadLine();
+                big = big.ToLower();
+
+                if (big == "lapo'a" || big == "lapoa")
+                {
+                    Console.Clear();
+                    Console.WriteLine("INPUT: " + big);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Nice job, Press enter to continue.");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    smol();
+                }
+                else if (big != "lapo'a" || big != "lapoa")
+                {
+                    Console.Clear();
+                    Console.WriteLine("INPUT: " + big);
+                    Console.WriteLine("That is incorrect. Press enter to try again");
+                    Console.ReadLine();
+                    BIG();
+                }    
+            }
+            static void smol()
+            {
+                Console.Clear();
+                Console.WriteLine("In Samoan, to say the word 'Small.'");
+                Console.WriteLine("");
+            }
         }
     }
 }

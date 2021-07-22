cc26000ae74aa89d3511580bd0912ac0a03a4723
Brooklyn Eti <ac106494@avcol.school.nz>
2021-07-22 18:25:06 +1200

    finished level five and started level five quiz

---
+++ EnglishToSamoan/FIVEQUIZcs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnglishToSamoan
+{
+    public class FIVEQUIZcs
+    {
+        public FIVEQUIZcs()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the final quiz for the Samoan Tutorials Point.");
+            Console.WriteLine("There will be 5 questions in this quiz.");
+            Console.WriteLine("there will be 3 multichoice questions.");
+            Console.WriteLine("and 2 Written answer questions");
+        }
+            
+    }
+}
